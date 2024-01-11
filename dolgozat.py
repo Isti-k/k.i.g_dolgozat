d418056295d74a1d729d491200e6892865f2214a
Isti-k <kiscsere03@gmail.com>
2024-01-11 10:10:39 +0100

    dolgozat

---
+++ dolgozat.py
@@ -0,0 +1,16 @@
+
+"""a)	Kérj be 1 páros számot a felhasználótól. (1 pont)
+    Amennyiben nem páros számot ad meg a felhasználó, akkor kérd be újra a számot, addig, amíg páros számot nem ad meg!  (1 pont)
+    A bekéréshez írj egy függvényt beker(), amely bekéri a kívánt feltételeknek megfelelő számot. . (1 pont)
+    """
+
+def paros_szam():
+    print("1. feladat:")
+    szam:int=int(input("Kérek egy páros számot!"))
+    while szam % 2 == 0:
+        if
+
+
+
+
+
