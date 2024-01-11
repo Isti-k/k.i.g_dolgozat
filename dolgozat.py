691cac4a71ac586867e75f0d6eec3520001217e2
Isti-k <kiscsere03@gmail.com>
2024-01-11 11:58:04 +0100

    vege

--- dolgozat.py
+++ dolgozat.py
@@ -3,8 +3,8 @@ import random
     Amennyiben nem páros számot ad meg a felhasználó, akkor kérd be újra a számot, addig, amíg páros számot nem ad meg!  (1 pont)
     A bekéréshez írj egy függvényt beker(), amely bekéri a kívánt feltételeknek megfelelő számot. . (1 pont)
     """
-
-def beker():
+paros_lista=[]
+def beker(paros_lista):
     print("1. feladat:")
     print("a")
     szam:int=int(input("Kérek egy páros számot!"))
@@ -12,23 +12,58 @@ def beker():
         szam:int=int(input("Ez nem páros! Páros számot kérek!"))
     return szam
 
-def beker2():
+def beker2(paros_lista):
     print("1. feladat:")
     print("b")
     i:int=1
     szam:int=int(input(f"Kérek az {i}. egy páros számot!"))
     for i in range(3): 
         if szam % 2 == 0:
-            szam+=1
-            return szam
+            i+=1
         else:
-            szam:int=int(input(f"Ez nem páros! {i+1} Páros számot kérek!"))
-    return szam
+            szam:int=int(input(f"Ez nem páros! {i+1}. Páros számot kérek!"))
+    return (szam * 3).append(paros_lista)
+
+
+def beker(paros_lista):
+    min:int=0
+    for i in range(0,len(paros_lista),1):
+        if paros_lista[i] <= paros_lista[min]:
+            min+=i
+    print(f"{i}. lépésben adta meg a legkisebb páros számot, melynek értéke:{min}")
+    return paros_lista
 
 
 lista=[]
 def veletlen():
-    szam1:random.randint(-40,150)
+    for i in range(0,13,1):
+        szam=random.randint(-40,150)
+        lista.append(szam)
+        if i > 12:
+            print(szam, end=",")
+        else:
+            print(szam,end=" ")
+
+def ketjegyuek_szama(lista):
+    osszeg=0
+    for i in range(0, len(lista), 1):
+        if 100 > lista[i] > 9 or -100 < lista[i] < -9:
+            osszeg += 1
+    return osszeg
     
+def paratlan_osszege(lista):
+    osszeg = 0
+    for i in range(0, len(lista), 1):
+        if lista[i] % 2 != 0:
+            osszeg += lista[i]
+    return osszeg
 
+def paros_osszege(lista):
+    osszeg = 0
+    for i in range(0, len(lista), 1):
+        if lista[i] % 2 == 0:
+            osszeg += lista[i]
+    return osszeg
 
+def kiir():
+    
--- main.py
+++ main.py
@@ -1,9 +1,19 @@
 import dolgozat
 
 
-paros=dolgozat.beker()
-print(f"\t{paros}")
+"""paros=dolgozat.beker()
+print(f"\t{paros}")"""
 
-hanyadik=dolgozat.beker2()
-print(f"\t{hanyadik}")
+"""hanyadik=dolgozat.beker2()
+print(f"\t{hanyadik}")"""
+
+random=dolgozat.veletlen()
+print(random)
+
+ketjegy=dolgozat.ketjegyuek_szama()
+print(f" A kétjegyűek számok száma:{ketjegy}")
+print("")
+paratlan=dolgozat.paratlan_osszege()
+paros=dolgozat.paros_osszege()
+print(f"A párosok öszege: {paros} > a páratlanok  összege: {paratlan}.")
 
